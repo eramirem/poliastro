9c6cf9ee31116b8bfa1b0f71cc1ec22335cf4136
eramirem <eramirem@gmail.com>
2017-03-30 21:08:00 -0400

    Initial commit PCP

---
+++ src/poliastro/porkchop.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+"""Porkchop Plots.
+
+"""
+
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from poliastro import ephem
+from poliastro.twobody import Orbit
+from poliastro.iod import lambert
